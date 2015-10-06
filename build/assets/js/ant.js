01371d92eece7c4bd75f420f1bbb96bfc7ec03e0
abrasumente <abrasumentee@gmail.com>
2015-10-06 23:05:35 +0800

    getting started

--- build/assets/js/ant.js
+++ build/assets/js/ant.js
@@ -1,7 +1,45 @@
-var LetItRun = React.createClass({displayName: "LetItRun",
-   render: function() {
-     return React.createElement("p", null, "It works!");
-   }
-});
+'use strict';
 
-React.render(React.createElement(LetItRun, null), document.getElementById('content'));
+(function () {
+    'use strict';
+
+    var height = 50,
+        width = 70,
+        center = { 'x': Math.floor(width / 2), 'y': Math.floor(height / 2) };
+    console.log(center);
+
+    var SquareLattice = React.createClass({ displayName: "SquareLattice",
+        render: function render() {
+            var rows = [];
+            for (var i = 0; i < height; i++) {
+                var cells = [];
+                for (var j = 0; j < width; j++) {
+                    cells[j] = React.createElement(Cell, { x: j, y: i });
+                }
+                rows[i] = React.createElement("div", { className: "row" }, cells);
+            }
+
+            console.log(rows);
+
+            return React.createElement("div", null, rows);
+        }
+    });
+
+    var Cell = React.createClass({ displayName: "Cell",
+        getInitialState: function getInitialState() {
+            if (this.props.x == 35) {
+                console.log(this.props.x, this.props.y);
+            }
+            status = this.props.x == center.x && this.props.y == center.y ? 'ant' : 'white';
+            return { status: status };
+        },
+        render: function render() {
+            var cx = React.addons.classSet;
+            var classes = cx('cell', this.state.status);
+            if (this.state.status === 'ant') console.log('it exists');
+            return React.createElement("div", { className: classes });
+        }
+    });
+
+    React.render(React.createElement(SquareLattice, null), document.getElementById('content'));
+})();
--- src/js/ant.jsx
+++ src/js/ant.jsx
@@ -1,7 +1,45 @@
-var LetItRun = React.createClass({
-   render: function() {
-     return <p>It works!</p>;
-   }
-});
+(function() {
+    'use strict';
 
-React.render(<LetItRun />, document.getElementById('content'));
+    var height = 50,
+        width = 70,
+        center = {'x': Math.floor(width/2), 'y': Math.floor(height/2)};
+    console.log(center);
+
+    var SquareLattice = React.createClass({
+       render: function() {
+           var rows = [];
+           for (var i = 0; i < height; i++) {
+               let cells = [];
+               for (var j = 0; j < width; j++) {
+                   cells[j] = <Cell x={j} y={i} />;
+               }
+               rows[i] = <div className="row">{cells}</div>;
+           }
+
+           console.log(rows);
+
+           return <div>{rows}</div>;
+       }
+    });
+
+    var Cell = React.createClass({
+        getInitialState: function() {
+            if (this.props.x == 35) {
+                console.log(this.props.x, this.props.y);
+            }
+            status = this.props.x == center.x &&
+                this.props.y == center.y ? 'ant' : 'white';
+            return {status: status};
+        },
+        render: function () {
+            var cx = React.addons.classSet;
+            var classes = cx('cell', this.state.status);
+            if (this.state.status === 'ant')
+                console.log('it exists');
+            return <div className={classes}></div>;
+        }
+    });
+
+    React.render(<SquareLattice />, document.getElementById('content'));
+})();
