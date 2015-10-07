7823ddedecf6cb619822136c44aa0ddb3536c034
abrasumente <abrasumentee@gmail.com>
2015-10-07 09:50:31 +0800

    first impl

--- build/assets/js/ant.js
+++ build/assets/js/ant.js
@@ -5,10 +5,36 @@
 
     var height = 50,
         width = 70,
-        center = { 'x': Math.floor(width / 2), 'y': Math.floor(height / 2) };
-    console.log(center);
+        center = { 'x': Math.floor(width / 2), 'y': Math.floor(height / 2) },
+        delay = 100,
+        directions = ['top', 'right', 'bottom', 'left'];
 
     var SquareLattice = React.createClass({ displayName: "SquareLattice",
+        getInitialState: function getInitialState() {
+            return {
+                antX: center.x,
+                antY: center.y,
+                antDirection: 3 // Default direction is left
+            };
+        },
+        componentDidMount: function componentDidMount() {
+            var move = (function () {
+
+                var ant = document.getElementsByClassName('ant')[0],
+                    className = ant.getAttribute('class'),
+                    right = className.indexOf('white') > -1;
+
+                // 不用理解这里的意思，知道白右转黑左转就行了！！！
+                var direction = right ? (this.state.antDirection + 1) % 4 : this.state.antDirection - 1 === -1 ? 3 : this.state.antDirection - 1,
+                    x = direction % 2 !== 0 ? direction === 1 ? this.state.antX + 1 : this.state.antX - 1 : this.state.antX,
+                    y = direction % 2 === 0 ? direction === 0 ? this.state.antY + 1 : this.state.antY - 1 : this.state.antY;
+
+                console.log(direction, x, y);
+
+                setTimeout(move, delay);
+            }).bind(this);
+            setTimeout(move, delay);
+        },
         render: function render() {
             var rows = [];
             for (var i = 0; i < height; i++) {
@@ -19,7 +45,7 @@
                 rows[i] = React.createElement("div", { className: "row" }, cells);
             }
 
-            console.log(rows);
+            this._rows = rows;
 
             return React.createElement("div", null, rows);
         }
@@ -27,16 +53,12 @@
 
     var Cell = React.createClass({ displayName: "Cell",
         getInitialState: function getInitialState() {
-            if (this.props.x == 35) {
-                console.log(this.props.x, this.props.y);
-            }
             status = this.props.x == center.x && this.props.y == center.y ? 'ant' : 'white';
             return { status: status };
         },
         render: function render() {
             var cx = React.addons.classSet;
             var classes = cx('cell', this.state.status);
-            if (this.state.status === 'ant') console.log('it exists');
             return React.createElement("div", { className: classes });
         }
     });
--- src/js/ant.jsx
+++ src/js/ant.jsx
@@ -1,42 +1,66 @@
 (function() {
     'use strict';
 
-    var height = 50,
-        width = 70,
-        center = {'x': Math.floor(width/2), 'y': Math.floor(height/2)};
-    console.log(center);
+    const height     = 50,
+          width      = 70,
+          center     = {'x': Math.floor(width/2), 'y': Math.floor(height/2)},
+          delay      = 100,
+          directions = ['top', 'right', 'bottom', 'left'];
 
-    var SquareLattice = React.createClass({
-       render: function() {
-           var rows = [];
-           for (var i = 0; i < height; i++) {
+    let SquareLattice = React.createClass({
+        getInitialState: function() {
+            return {
+                antX: center.x,
+                antY: center.y,
+                antDirection: 3 // Default direction is left
+            };
+        },
+        componentDidMount: function() {
+            let move = function() {
+
+                let ant = document.getElementsByClassName('ant')[0],
+                    className = ant.getAttribute('class'),
+                    right = className.indexOf('white') > -1;
+
+                // 不用理解这里的意思，知道白右转黑左转就行了！！！
+                let direction = right ? (this.state.antDirection + 1) % 4
+                        : ((this.state.antDirection - 1 === -1) ? 3 : this.state.antDirection - 1),
+                    x = direction % 2 !== 0 ? (direction === 1 ? this.state.antX + 1 : this.state.antX - 1)
+                        : this.state.antX,
+                    y = direction % 2 === 0 ? (direction === 0 ? this.state.antY + 1 : this.state.antY - 1)
+                        : this.state.antY;
+
+                console.log(direction, x, y);
+
+                setTimeout(move, delay);
+            }.bind(this);
+            setTimeout(move, delay);
+        },
+        render: function() {
+            let rows = [];
+            for (let i = 0; i < height; i++) {
                let cells = [];
-               for (var j = 0; j < width; j++) {
+               for (let j = 0; j < width; j++) {
                    cells[j] = <Cell x={j} y={i} />;
                }
                rows[i] = <div className="row">{cells}</div>;
-           }
+            }
 
-           console.log(rows);
+            this._rows = rows;
 
-           return <div>{rows}</div>;
+            return <div>{rows}</div>;
        }
     });
 
-    var Cell = React.createClass({
+    let Cell = React.createClass({
         getInitialState: function() {
-            if (this.props.x == 35) {
-                console.log(this.props.x, this.props.y);
-            }
             status = this.props.x == center.x &&
                 this.props.y == center.y ? 'ant' : 'white';
             return {status: status};
         },
         render: function () {
-            var cx = React.addons.classSet;
-            var classes = cx('cell', this.state.status);
-            if (this.state.status === 'ant')
-                console.log('it exists');
+            let cx = React.addons.classSet;
+            let classes = cx('cell', this.state.status);
             return <div className={classes}></div>;
         }
     });
