6d3f47999b156bac0e700a481bee94c2de9f5df6
abrasumente <abrasumentee@gmail.com>
2015-10-07 10:45:34 +0800

    first version.

--- build/assets/js/ant.js
+++ build/assets/js/ant.js
@@ -6,15 +6,26 @@
     var height = 50,
         width = 70,
         center = { 'x': Math.floor(width / 2), 'y': Math.floor(height / 2) },
-        delay = 100,
+        delay = 500,
         directions = ['top', 'right', 'bottom', 'left'];
 
     var SquareLattice = React.createClass({ displayName: "SquareLattice",
         getInitialState: function getInitialState() {
+
+            // initial work of this.state.color
+            var color = [];
+            for (var x = 0; x < width; x++) {
+                color[x] = [];
+                for (var y = 0; y < width; y++) {
+                    color[x][y] = false; // False means white, true means black
+                }
+            }
+
             return {
                 antX: center.x,
                 antY: center.y,
-                antDirection: 3 // Default direction is left
+                antDirection: 3, // Default direction is left,
+                color: color
             };
         },
         componentDidMount: function componentDidMount() {
@@ -27,7 +38,11 @@
                 // 不用理解这里的意思，知道白右转黑左转就行了！！！
                 var direction = right ? (this.state.antDirection + 1) % 4 : this.state.antDirection - 1 === -1 ? 3 : this.state.antDirection - 1,
                     x = direction % 2 !== 0 ? direction === 1 ? this.state.antX + 1 : this.state.antX - 1 : this.state.antX,
-                    y = direction % 2 === 0 ? direction === 0 ? this.state.antY + 1 : this.state.antY - 1 : this.state.antY;
+                    y = direction % 2 === 0 ? direction === 0 ? this.state.antY - 1 : this.state.antY + 1 : this.state.antY;
+
+                console.log(direction, x, y);
+
+                this.state.color[this.state.antX][this.state.antY] = !this.state.color[this.state.antX][this.state.antY];
 
                 this.setState({
                     antX: x,
@@ -44,7 +59,7 @@
             for (var y = 0; y < height; y++) {
                 var cells = [];
                 for (var x = 0; x < width; x++) {
-                    cells[x] = React.createElement(Cell, { ant: x === this.state.antX && y === this.state.antY });
+                    cells[x] = React.createElement(Cell, { ant: x === this.state.antX && y === this.state.antY, color: this.state.color[x][y] ? 'black' : 'white' });
                 }
                 rows[y] = React.createElement("div", { className: "row" }, cells);
             }
@@ -55,7 +70,8 @@
 
     var Cell = React.createClass({ displayName: "Cell",
         propTypes: {
-            ant: React.PropTypes.bool.isRequired
+            ant: React.PropTypes.bool.isRequired,
+            color: React.PropTypes.string.isRequired
         },
         getInitialState: function getInitialState() {
             var ant = this.props.x == center.x && this.props.y == center.y,
@@ -64,7 +80,7 @@
         },
         render: function render() {
             var cx = React.addons.classSet;
-            var classes = cx('cell', this.state.status, this.props.ant ? 'ant' : '');
+            var classes = cx('cell', this.props.color, this.props.ant ? 'ant' : '');
             if (this.props.ant) {
                 console.log('ok');
             }
--- src/js/ant.jsx
+++ src/js/ant.jsx
@@ -4,15 +4,26 @@
     const height     = 50,
           width      = 70,
           center     = {'x': Math.floor(width/2), 'y': Math.floor(height/2)},
-          delay      = 100,
+          delay      = 500,
           directions = ['top', 'right', 'bottom', 'left'];
 
     let SquareLattice = React.createClass({
         getInitialState: function() {
+
+            // initial work of this.state.color
+            let color = [];
+            for (let x = 0; x < width; x++) {
+                color[x] = [];
+                for (let y = 0; y < width; y++) {
+                    color[x][y] = false; // False means white, true means black
+                }
+            }
+
             return {
                 antX: center.x,
                 antY: center.y,
-                antDirection: 3 // Default direction is left
+                antDirection: 3, // Default direction is left,
+                color: color
             };
         },
         componentDidMount: function() {
@@ -22,13 +33,19 @@
                     className = ant.getAttribute('class'),
                     right = className.indexOf('white') > -1;
 
+
+
                 // 不用理解这里的意思，知道白右转黑左转就行了！！！
                 let direction = right ? (this.state.antDirection + 1) % 4
                         : ((this.state.antDirection - 1 === -1) ? 3 : this.state.antDirection - 1),
                     x = direction % 2 !== 0 ? (direction === 1 ? this.state.antX + 1 : this.state.antX - 1)
                         : this.state.antX,
-                    y = direction % 2 === 0 ? (direction === 0 ? this.state.antY + 1 : this.state.antY - 1)
+                    y = direction % 2 === 0 ? (direction === 0 ? this.state.antY - 1 : this.state.antY + 1)
                         : this.state.antY;
+
+                console.log(direction, x, y);
+
+                this.state.color[this.state.antX][this.state.antY] = !this.state.color[this.state.antX][this.state.antY]
 
                 this.setState({
                     antX: x,
@@ -45,7 +62,7 @@
             for (let y = 0; y < height; y++) {
                let cells = [];
                for (let x = 0; x < width; x++) {
-                   cells[x] = <Cell ant={x === this.state.antX && y === this.state.antY} />;
+                   cells[x] = <Cell ant={x === this.state.antX && y === this.state.antY} color={this.state.color[x][y] ? 'black' : 'white'} />;
                }
                rows[y] = <div className="row">{cells}</div>;
             }
@@ -56,7 +73,8 @@
 
     let Cell = React.createClass({
         propTypes: {
-          ant:  React.PropTypes.bool.isRequired
+            ant:  React.PropTypes.bool.isRequired,
+            color: React.PropTypes.string.isRequired
         },
         getInitialState: function() {
             let ant = this.props.x == center.x && this.props.y == center.y,
@@ -65,7 +83,7 @@
         },
         render: function () {
             let cx = React.addons.classSet;
-            let classes = cx('cell', this.state.status, this.props.ant ? 'ant' : '');
+            let classes = cx('cell', this.props.color, this.props.ant ? 'ant' : '');
             if (this.props.ant) {
                 console.log('ok');
             }
